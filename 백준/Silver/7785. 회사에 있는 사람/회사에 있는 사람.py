755a7f20aaead1fb06b040f16c302a7455ee90aa
Lim Seongjoo <121420744+Imseongjoo@users.noreply.github.com>
2023-02-03 20:45:55 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 660 ms, Memory: 169096 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,13 @@
+n = int(input())
+dic = {}
+for i in range(n):
+    name, log = input().split()
+    dic[name] = log
+    if log == "leave":
+        del dic[name]
+
+d = sorted(dic.items(), reverse=True)
+dic = dict(d)
+
+for key in dic.keys():
+    print(key)
