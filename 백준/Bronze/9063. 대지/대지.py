1b54cf7267c2ef7d3260e3e7a9286c1ef0a4ad80
Lim Seongjoo <121420744+Imseongjoo@users.noreply.github.com>
2023-04-21 17:29:33 +0900

    [Bronze III] Title: 대지, Time: 92 ms, Memory: 35196 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+x_list = []
+y_list = []
+for _ in range(int(input())) :
+    x, y = map(int, input().split())
+    x_list.append(x)
+    y_list.append(y)
+print((max(x_list) - min(x_list)) * (max(y_list) - min(y_list)))
